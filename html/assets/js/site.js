c6dcef4e0614cd139586747f28dd4ab12bbe1c93
Muzaffer Hassan <muzaffer.hasan@gmail.com>
2017-11-25 19:43:26 +0530

    after responsive of reputation page

--- html/assets/js/site.js
+++ html/assets/js/site.js
@@ -1,5 +1,12 @@
-
+// $(window).load(function(){
+//     if($window.width()<768){
+        
+//     }
+// })
 $(document).ready(function(){
+    if($(window).width()<768){
+        $('header.header, .content').toggleClass('close-menu'); 
+    }
     $('.accord-header').click(function(){
         var $accordBody = $(this).next();
         $(this).toggleClass('open');
@@ -56,7 +63,6 @@ $(document).ready(function(){
         var values = options.values;
         var colors = options.colors;
         var legends = options. legends;
-        console.log(options);
         var html = `<ul class="attribute-list">`;
         for(var i = 0; i< labels.length; i++){
             html += `
@@ -89,6 +95,123 @@ $(document).ready(function(){
     $('.scroll-to-top').click(function(){
         $('html,body').animate({scrollTop:0},500);
     })
+
+    /*Circle Progress*/
+    var myCanvas = document.getElementById('my_canvas');
+    var myCanvas2 = document.getElementById('my_canvas2');
+        var p1=0.70,p2=0.75,p3=0.8,headingSize=30,sublineSize=14,min_Radius=45,gap_Width = 10
+        arc_Width = 15, center_x = 115, center_y=140, infoLine_Len = 150, infoLine_Hor=50;
+        
+        if($(window).width()<1250){
+            headingSize = 20;
+            sublineSize = 12;
+            myCanvas.setAttribute("width","300");
+            myCanvas2.setAttribute("width","300");
+            min_Radius = 30;
+            gap_Width = 5;
+            arc_Width = 10;
+            center_x = 95;
+            center_y=110;
+            infoLine_Len = 100;
+            infoLine_Hor=50;
+        }
+        if($(window).width()<768){
+            headingSize = 14;
+            sublineSize = 12;
+            myCanvas.setAttribute("width",$(window).width()-85);
+            myCanvas.setAttribute("height",175);
+            myCanvas2.setAttribute("width",$(window).width()-85);
+            myCanvas2.setAttribute("height",175);
+            min_Radius = 15;
+            gap_Width = 3;
+            arc_Width = 5;
+            center_x = 50;
+            center_y=110;
+            infoLine_Len = 90;
+            infoLine_Hor=30;
+        }
+        
+        var circle = new ProgressCircle({
+            canvas: myCanvas,
+            minRadius: min_Radius, // Inner radius of the innermost circle
+            arcWidth: arc_Width, // Width of each circle
+            gapWidth: gap_Width, // Space between adjacent circles
+            centerX: center_x, // X coordinate of the circle center
+            centerY: center_y, // Y coordinate of the circle center
+            infoLineLength: infoLine_Len, // Length of the info line
+            horizLineLength: infoLine_Hor, // Length of the horizontal info line
+            infoLineBaseAngle: Math.PI / 6, // Start angle of the info line
+            infoLineAngleInterval: Math.PI / 8, // Angle between info lines
+        });
+
+        
+        
+        circle.addEntry({
+            fillColor: 'rgba(194, 227, 239, 1)',
+            outlineColor: 'rgba(194, 227, 239, 1)', // (Optional)
+            progressListener: function() {return p1;},
+            infoListener: function() {return `<h3 style="font-size:${headingSize}px; color:#00b0f0;">
+                ${p1*100}% <small style="font-size:${sublineSize}px;">Highest Quality Output</small>
+                </h3>`;},// (Optional)
+        }).addEntry({
+            fillColor: 'rgba(133, 211, 240, 1)',
+            outlineColor: 'rgba(133, 211, 240, 1)', // (Optional)
+            progressListener: function() {return p2;},
+            infoListener: function() {return `<h3 style="font-size:${headingSize}px; color:#00b0f0;">
+                ${p2*100}% <small style="font-size:${sublineSize}px;">Highest Quality Output</small>
+                </h3>`;},// (Optional)
+        }).addEntry({
+            fillColor: 'rgba(0, 176, 240, 1)',
+            outlineColor: 'rgba(0, 176, 240, 1)', // (Optional)
+            progressListener: function() {return p3;},
+            infoListener: function() {return `<h3 style="font-size:${headingSize}px; color:#00b0f0;">
+                ${p3*100}% <small style="font-size:${sublineSize}px;">Highest Quality Output</small>
+                </h3>`;},// (Optional)
+        });
+        circle.start();
+        setTimeout(function(){
+            circle.stop();
+            circle2.stop();
+        },1000);
+
+        var c2p1=0.48,c2p2=0.30,c2p3=0.18;
+        var circle2 = new ProgressCircle({
+            canvas: myCanvas2,
+            minRadius: min_Radius, // Inner radius of the innermost circle
+            arcWidth: arc_Width, // Width of each circle
+            gapWidth: gap_Width, // Space between adjacent circles
+            centerX: center_x, // X coordinate of the circle center
+            centerY: center_y, // Y coordinate of the circle center
+            infoLineLength: infoLine_Len, // Length of the info line
+            horizLineLength: infoLine_Hor, // Length of the horizontal info line
+            infoLineBaseAngle: Math.PI / 6, // Start angle of the info line
+            infoLineAngleInterval: Math.PI / 8, // Angle between info lines
+        });
+
+        circle2.addEntry({
+            fillColor: 'rgba(189, 188, 188, 1)',
+            outlineColor: 'rgba(189, 188, 188, 1)', // (Optional)
+            progressListener: function() {return c2p1;},
+            infoListener: function() {return `<h3 style="font-size:${headingSize}px; color:#787878;">
+                ${c2p1*100}%  <small style="font-size:${sublineSize}px; margin-top:-15px;">Highly Collaborative</small>
+                </h3>`;},// (Optional)
+        }).addEntry({
+            fillColor: 'rgba(155, 154, 154, 1)',
+            outlineColor: 'rgba(155, 154, 154, 1)', // (Optional)
+            progressListener: function() {return c2p2;},
+            infoListener: function() {return `<h3 style="font-size:${headingSize}px; color:#787878;">
+                ${c2p2*100}%  <small style="font-size:${sublineSize}px; margin-top:-15px;">Priority/Time Mngt</small>
+                </h3>`;},// (Optional)
+        }).addEntry({
+            fillColor: 'rgba(110, 110, 110, 1)',
+            outlineColor: 'rgba(110, 110, 110, 1)', // (Optional)
+            progressListener: function() {return c2p3;},
+            infoListener: function() {return `<h3 style="font-size:${headingSize}px; color:#787878;">
+                ${c2p3*100}%  <small style="font-size:${sublineSize}px; margin-top:-50px;">Client Facing</small>
+                </h3>`;},// (Optional)
+        });
+        circle2.start();
+        
 })
 
 $(window).scroll(function(){
